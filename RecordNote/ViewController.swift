fc5e67cc3cce535edd082c888133253878831988
mohamed930 <mohammedali12477@gmail.com>
2026-05-26 16:04:48 +0300

    Initial Commit

---
+++ RecordNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecordNote
+//
+//  Created by Mohamed Ali on 26/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
