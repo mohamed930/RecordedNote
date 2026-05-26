2bb2f2072e326773845521ed5152b2098079fd61
mohamed930 <mohammedali12477@gmail.com>
2026-05-26 18:09:24 +0300

    Add dependency container

---
+++ RecordNote/Dependency container/DIContainer.swift
@@ -0,0 +1,16 @@
+//
+//  DIContainer.swift
+//  RecordNote
+//
+//  Created by Mohamed Ali on 26/05/2026.
+//
+
+import Foundation
+
+final class DIContainer {
+
+    static let shared = DIContainer()
+
+    private init() {}
+
+}
