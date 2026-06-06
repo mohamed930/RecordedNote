97ea8efd52fc7589f79a654b070237f344bea029
mohamed930 <mohammedali12477@gmail.com>
2026-06-06 17:14:38 +0300

    Make Home Screen UI

--- RecordNote/Presentation Layer/TabBar Module/HomeScreen/View/HomeView.swift
+++ RecordNote/Presentation Layer/TabBar Module/HomeScreen/View/HomeView.swift
@@ -19,9 +19,80 @@ struct HomeView: View {
     }
 
     var body: some View {
-        VStack {
-            Text("Home")
+        VStack(alignment: .leading,spacing: 0) {
+            
+            HStack(spacing: 0) {
+                Text("Good Morning ")
+                
+                Text("\(viewModel.userName) ")
+                
+                Image(.hi)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20)
+                
+                Spacer()
+            }
+            .setFont(fontName: .mainFontBold, size: 20)
+            .padding(.bottom,3.5)
+            
+            Text("Let's capture your thoughts")
+                .setFont(fontName: .mainFont, size: 16)
+                .foregroundStyle(Color._6_A_7282)
+                .padding(.bottom,32)
+            
+            VStack(alignment: .center,spacing: 0) {
+                Button {
+                    viewModel.recordNoteButtonAction()
+                } label: {
+                    Image(.recordButton)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 192)
+                        .blur(radius: 0.9)
+                }
+                
+                Text("Tap to start recording")
+                    .setFont(fontName: .mainFontMeduim, size: 16)
+                    .foregroundStyle(Color._6_A_7282)
+
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.bottom,60)
+            
+            HStack {
+                Text("Recent Notes")
+                    .setFont(fontName: .mainFontBold, size: 18)
+                
+                Spacer()
+                
+                Button {
+                    viewModel.seeAllButtonAction()
+                } label: {
+                    HStack(spacing: 2) {
+                        Text("See All")
+                            .setFont(fontName: .mainFont, size: 14)
+                            .foregroundStyle(Color._7_C_3_AED)
+                        
+                        Image(.moreButton)
+                    }
+                }
+
+            }
+            .padding(.bottom,16)
+            
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(viewModel.notes) { note in
+                        MeetingNoteCard(note: note)
+                    }
+                }
+            }
+            
+            Spacer()
         }
+        .padding(.top,10)
+        .padding(.horizontal,24)
     }
 }
 
--- RecordNote/Presentation Layer/TabBar Module/HomeScreen/ViewModel/HomeViewModel.swift
+++ RecordNote/Presentation Layer/TabBar Module/HomeScreen/ViewModel/HomeViewModel.swift
@@ -7,11 +7,37 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 final class HomeViewModel: ObservableObject {
     private weak var coordinator: HomeCoordinator?
+    
+    @Published var userName: String = "Mohamed Ali"
+    
+    @Published var notes: [MeetingNote] = [
+                                                .init(
+                                                    title: "Project Meeting Notes",
+                                                    date: Date(),
+                                                    indicatorColor: .purple
+                                                ),
+                                                .init(
+                                                    title: "Sprint Planning",
+                                                    date: Date().addingTimeInterval(3600),
+                                                    indicatorColor: .blue
+                                                )
+                                            ]
 
     init(coordinator: HomeCoordinator) {
         self.coordinator = coordinator
+        
+    }
+    
+    // MARK: - Actions.
+    func recordNoteButtonAction() {
+        
+    }
+    
+    func seeAllButtonAction() {
+        
     }
 }
